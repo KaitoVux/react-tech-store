6a019b0999d27c4f3a016fcbd78701aaff569ffb
KaitoVux <39855103+KaitoVux@users.noreply.github.com>
2020-11-06 14:54:13 +0700

    add product details component

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,5 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import Loading from "../components/Loading";
+import { ProductContext } from "../context/products";
 
 export default function ProductDetails() {
-  return <h1>hello from product details page</h1>;
+  const { id } = useParams();
+  const history = useHistory();
+
+  const { products } = useContext(ProductContext);
+  const product = products.find((item) => item.id === parseInt(id));
+
+  if (products.length === 0) {
+    return <Loading />;
+  }
+
+  const {
+    image: { url },
+    price,
+    title,
+    description,
+  } = product;
+
+  return (
+    <section className="single-product">
+      <img src={url} alt={title} className="single-product-image" />
+      <article>
+        <h1>{title}</h1>
+        <h2>${price}</h2>
+        <p>{description}</p>
+        <button
+          className="btn btn-primary btn-block"
+          onClick={() => history.push("/cart")}
+        >
+          add to cart
+        </button>
+      </article>
+    </section>
+  );
 }
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import Loading from "../components/Loading";
 import ProductList from "../components/Products/ProductList";
 import { ProductContext } from "../context/products";
+
 export default function Products() {
   const store = useContext(ProductContext);
   const { loading, products, featured } = store;
